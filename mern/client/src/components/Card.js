c57b934487a39a81d26268dada3b3096779d55f1
Nathan Wong <natdanwongielhan@yahoo.com>
2023-07-26 21:29:52 -0700

    prepare to work on symbols

--- mern/client/src/components/Card.js
+++ mern/client/src/components/Card.js
@@ -1,7 +1,11 @@
 import "./Card.css";
 
 function Card() {
-  return <div className="Card"></div>;
+  return (
+    <div className="Card">
+      <div className="CardSymbol">HELLO</div>
+    </div>
+  );
 }
 
 export default Card;
