f29eaa618cd83bd4dd6e005b6885f09fb7f6f477
Nathan Wong <natdanwongielhan@yahoo.com>
2023-07-28 18:59:49 -0700

    Styled Squiggle to acceptable basic shape

--- mern/client/src/components/Symbols/Squiggle.js
+++ mern/client/src/components/Symbols/Squiggle.js
@@ -1,7 +1,18 @@
 import "./Squiggle.css";
 
 function Squiggle() {
-  return <div className="Squiggle"></div>;
+  return (
+    <div className="Squiggle">
+      <div className="Row" id="RowTop">
+        <div className="Squiggle1" />
+        <div className="Squiggle2" />
+      </div>
+      <div className="Row" id="RowBot">
+        <div className="Squiggle3" />
+        <div className="Squiggle4" />
+      </div>
+    </div>
+  );
 }
 
 export default Squiggle;
