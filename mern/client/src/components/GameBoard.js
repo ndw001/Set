4e116bb54482792f3d37bd57c09d645813c285c1
Nathan Wong <natdanwongielhan@yahoo.com>
2023-07-30 19:50:45 -0700

    add multiple examples to gameboard

--- mern/client/src/components/GameBoard.js
+++ mern/client/src/components/GameBoard.js
@@ -15,8 +15,13 @@ function GameBoard() {
       <Card shape={"Oval"} color={"Green"} fill={"Solid"} />
       <Card shape={"Oval"} color={"Purple"} fill={"Lines"} />
 
-      <Card shape={"Diamond"} color={"Green"} fill={"Empty"} />
-      <Card shape={"Squiggle"} color={"Purple"} fill={"Empty"} />
+      <Card shape={"Diamond"} color={"Red"} fill={"Lines"} />
+      <Card shape={"Diamond"} color={"Green"} fill={"Lines"} />
+      <Card shape={"Diamond"} color={"Purple"} fill={"Lines"} />
+
+      <Card shape={"Squiggle"} color={"Red"} fill={"Solid"} />
+      <Card shape={"Squiggle"} color={"Green"} fill={"Empty"} />
+      <Card shape={"Squiggle"} color={"Purple"} fill={"Solid"} />
     </div>
   );
 }
