ba340f6191cba8f502a1cfbfa78f054370751e08
Nathan Wong <natdanwongielhan@yahoo.com>
2023-08-03 17:06:19 -0700

    fix gameboard spacing to render in 4x3 grid

--- mern/client/src/components/GameBoard.js
+++ mern/client/src/components/GameBoard.js
@@ -11,17 +11,98 @@ function GameBoard() {
   // One, Two, Three
   return (
     <div className="GameBoard">
-      <Card shape={"Oval"} color={"Red"} fill={"Empty"} count={1} />
-      <Card shape={"Oval"} color={"Green"} fill={"Solid"} count={2} />
-      <Card shape={"Oval"} color={"Purple"} fill={"Lines"} count={3} />
+      <div className="CardRow">
+        <Card
+          className="Card"
+          shape={"Oval"}
+          color={"Red"}
+          fill={"Empty"}
+          count={1}
+        />
+        <Card
+          className="Card"
+          shape={"Oval"}
+          color={"Green"}
+          fill={"Solid"}
+          count={2}
+        />
+        <Card
+          className="Card"
+          shape={"Oval"}
+          color={"Purple"}
+          fill={"Lines"}
+          count={3}
+        />
 
-      <Card shape={"Diamond"} color={"Red"} fill={"Empty"} count={1} />
-      <Card shape={"Diamond"} color={"Green"} fill={"Lines"} count={2} />
-      <Card shape={"Diamond"} color={"Purple"} fill={"Solid"} count={3} />
+        <Card
+          className="Card"
+          shape={"Diamond"}
+          color={"Red"}
+          fill={"Empty"}
+          count={1}
+        />
+      </div>
+      <div className="CardRow">
+        <Card
+          className="Card"
+          shape={"Diamond"}
+          color={"Green"}
+          fill={"Lines"}
+          count={2}
+        />
+        <Card
+          className="Card"
+          shape={"Diamond"}
+          color={"Purple"}
+          fill={"Solid"}
+          count={3}
+        />
 
-      <Card shape={"Squiggle"} color={"Red"} fill={"Empty"} count={1} />
-      <Card shape={"Squiggle"} color={"Green"} fill={"Lines"} count={2} />
-      <Card shape={"Squiggle"} color={"Purple"} fill={"Solid"} count={3} />
+        <Card
+          className="Card"
+          shape={"Squiggle"}
+          color={"Red"}
+          fill={"Empty"}
+          count={1}
+        />
+        <Card
+          className="Card"
+          shape={"Squiggle"}
+          color={"Green"}
+          fill={"Lines"}
+          count={2}
+        />
+      </div>
+      <div className="CardRow">
+        <Card
+          className="Card"
+          shape={"Squiggle"}
+          color={"Purple"}
+          fill={"Solid"}
+          count={3}
+        />
+        <Card
+          className="Card"
+          shape={"Diamond"}
+          color={"Red"}
+          fill={"Solid"}
+          count={3}
+        />
+        <Card
+          className="Card"
+          shape={"Oval"}
+          color={"Purple"}
+          fill={"Empty"}
+          count={3}
+        />
+        <Card
+          className="Card"
+          shape={"Squiggle"}
+          color={"Green"}
+          fill={"Solid"}
+          count={1}
+        />
+      </div>
     </div>
   );
 }
