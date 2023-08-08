0086223b9ac05f68b25ea172a3edf443d620c24e
Nathan Wong <natdanwongielhan@yahoo.com>
2023-08-07 18:12:23 -0700

    remove and replace first implementation (need to fix undef, duplicate appears)

--- mern/client/src/components/GameBoard.js
+++ mern/client/src/components/GameBoard.js
@@ -1,20 +1,34 @@
 import "./GameBoard.css";
 import Card from "./Card";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import populateDeck from "./helperFunctions";
 
 const _ = require("lodash");
 
 function GameBoard(props) {
-  const remainingDeck = {};
+  const allCards = populateDeck();
+  const currentDeck = createDeckOrder([]);
+  const [rowOne, setRowOne] = useState([]);
+  const [rowTwo, setRowTwo] = useState([]);
+  const [rowThree, setRowThree] = useState([]);
+  const [rowFour, setRowFour] = useState([]);
+  const [rowFive, setRowFive] = useState([]);
+
   const [selectedCards, setSelectedCards] = useState([]);
   const [alert, setAlert] = useState("");
   const [points, setPoints] = useState(0);
 
-  function endCheck(isSet) {
+  useEffect(() => {
+    firstBoard();
+  }, []);
+
+  function endCheck(isSet, cardOneNumber, cardTwoNumber, cardThreeNumber) {
     if (isSet) {
       setAlert("This is a Set");
       setPoints(points + 1);
-      // Remove Cards from Board and add 3
+      removeAndReplaceCard(cardOneNumber);
+      removeAndReplaceCard(cardTwoNumber);
+      removeAndReplaceCard(cardThreeNumber);
       setSelectedCards([]);
     } else {
       setAlert("This is NOT a Set");
@@ -22,6 +36,55 @@ function GameBoard(props) {
       setSelectedCards([]);
 
       return;
+    }
+  }
+
+  function removeAndReplaceCard(cardNumber) {
+    let found = 0;
+    if (found !== 3) {
+      for (let i = 0; i < 3; i++) {
+        if (rowOne[i].cardNumber === cardNumber) {
+          const newCardNumber = currentDeck.pop();
+          const newCard = allCards.filter(
+            (card) => card.cardNumber === newCardNumber
+          )[0];
+          rowOne.splice(i, 1, newCard);
+          setRowOne(rowOne);
+          found += 1;
+        } else if (rowTwo[i].cardNumber === cardNumber) {
+          const newCardNumber = currentDeck.pop();
+          const newCard = allCards.filter(
+            (card) => card.cardNumber === newCardNumber
+          )[0];
+          rowTwo.splice(i, 1, newCard);
+          setRowTwo(rowTwo);
+          found += 1;
+        } else if (rowThree[i].cardNumber === cardNumber) {
+          const newCardNumber = currentDeck.pop();
+          const newCard = allCards.filter(
+            (card) => card.cardNumber === newCardNumber
+          )[0];
+          rowThree.splice(i, 1, newCard);
+          setRowThree(rowThree);
+          found += 1;
+        } else if (rowFour[i].cardNumber === cardNumber) {
+          const newCardNumber = currentDeck.pop();
+          const newCard = allCards.filter(
+            (card) => card.cardNumber === newCardNumber
+          )[0];
+          rowFour.splice(i, 1, newCard);
+          setRowFour(rowFour);
+          found += 1;
+        } else if (rowFive[i].cardNumber === cardNumber) {
+          const newCardNumber = currentDeck.pop();
+          const newCard = allCards.filter(
+            (card) => card.cardNumber === newCardNumber
+          )[0];
+          rowFive.splice(i, 1, newCard);
+          setRowFive(rowFive);
+          found += 1;
+        }
+      }
     }
   }
 
@@ -76,7 +139,12 @@ function GameBoard(props) {
       endCheck(false);
       return;
     }
-    endCheck(true);
+    endCheck(
+      true,
+      arrayOfCards[0].cardNumber,
+      arrayOfCards[1].cardNumber,
+      arrayOfCards[2].cardNumber
+    );
     return;
   }
 
@@ -95,14 +163,72 @@ function GameBoard(props) {
     if (currentCards.length === 3) {
       checkSet(currentCards);
     }
+  }
 
-    console.log("These are current Cards ", currentCards);
+  function createDeckOrder(deck) {
+    if (deck.length < 82) {
+      // WHY DOES 81 NOT WORK
+      const newNum = Math.floor(Math.random() * 82) + 1;
+      if (deck.find((num) => num === newNum)) {
+        createDeckOrder(deck);
+      } else {
+        deck.push(newNum);
+        createDeckOrder(deck);
+        return deck;
+      }
+    } else {
+      return deck;
+    }
+  }
+
+  function getThreeCards(currentDeck, deckOrder) {
+    const threeCards = [];
+    for (let k = 0; k < 3; k++) {
+      const searchFor = deckOrder.pop();
+      threeCards.push(
+        currentDeck.filter((card) => card.cardNumber === searchFor)[0]
+      );
+    }
+    return threeCards;
+  }
+
+  function populateRow(oneRow, num) {
+    return (
+      <div className="CardRow">
+        {oneRow.map((card) => (
+          <Card
+            className="Card"
+            shape={card.shape}
+            color={card.color}
+            fill={card.fill}
+            count={card.count}
+            cardNumber={card.cardNumber}
+            click={selectCard}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  function firstBoard() {
+    setRowOne(getThreeCards(allCards, currentDeck));
+    setRowTwo(getThreeCards(allCards, currentDeck));
+    setRowThree(getThreeCards(allCards, currentDeck));
+    setRowFour(getThreeCards(allCards, currentDeck));
+    setRowFive(getThreeCards(allCards, currentDeck));
   }
 
   return (
     <div className="GameBoard">
       <div className="Points">Points: {points} </div>
       <div className="Alerts">{alert}</div>
+      {populateRow(rowOne, 1)}
+      {populateRow(rowTwo, 2)}
+      {populateRow(rowThree, 3)}
+      {populateRow(rowFour, 4)}
+      {populateRow(rowFive, 5)}
+
+      {/*
       <div className="CardRow">
         <Card
           className="Card"
@@ -206,7 +332,7 @@ function GameBoard(props) {
           count={3}
           click={selectCard}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
